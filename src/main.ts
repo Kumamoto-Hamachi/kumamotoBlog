45b8a6bf2ae2eb70c3231e651a10928cac3b5a95
kumamoto <onioni9999nagare@gmail.com>
2023-12-11 00:31:26 +0900

    set up ElementPlus

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import './assets/main.css'
-import 'nes.css/css/nes.min.css'; // NES.cssをインポート
+import ElementPlus from "element-plus";
+import 'nes.css/css/nes.min.css';
+import 'element-plus/dist/index.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -8,5 +10,6 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
